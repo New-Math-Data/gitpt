74c00d3c7c94922e73ba01e9951f6407b5e889c2
larissa <llayne@newmathdata.com>
2024-10-22 13:57:39 -0500

    adds files and reorganizes test_git_commit structure

--- test_git_commit.py
+++ test_git_commit.py
@@ -2,10 +2,8 @@ from langchain_core.prompts import PromptTemplate
 from langchain_ollama.llms import OllamaLLM
 import sys
 
-def generate_commit_message(git_diff):
-    model= OllamaLLM(model="gemma2",temperature=0, top_p=0.5, top_k=10)
 
-    prompt_template = """
+prompt_1 = """
     Write concise, informative commit messages based on git diff: 
     Start with a summary in imperative mood, explain the 'why' behind changes, 
     keep the summary under 50 characters. 
@@ -14,19 +12,30 @@ def generate_commit_message(git_diff):
     git_diff: {git_diff}
     """
 
+
+def generate_commit_message(git_diff, prompt_txt):
+
+    model = OllamaLLM(model="gemma2", temperature=0, top_p=0.5, top_k=10)
+
     prompt = PromptTemplate(
         input_variables=["git_diff"],
-        template=prompt_template
+        template=prompt_txt
     )
 
-    code_summary_chain  = prompt | model
+    code_summary_chain = prompt | model
     commit_message = code_summary_chain.invoke(git_diff)
+
     return commit_message
 
 
 if __name__ == "__main__":
-  #git_diff = sys.argv[1]
-  file = open("diff.txt", "r")
-  git_diff = file.read()
-  file.close()
-  print(generate_commit_message(git_diff))
+
+    prompt_style = sys.argv[1]
+
+    file = open("test_data/diff.txt", "r")
+    diff_input = file.read()
+    file.close()
+
+    commit_msg = generate_commit_message(diff_input, prompt_style)
+
+    print(commit_msg)
