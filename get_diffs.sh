509e179850d12c3020f2ea81b6ee5f70307876ef
tatiana <tatiana@MacBook-Pro-3.local>
2024-10-23 16:06:45 -0600

    minor fixes

--- get_diffs.sh
+++ get_diffs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-diff_output=$(git diff)
-new_files=$(git status | awk '/new file:/ {print $NF}')
-renamed_files=$(git status | awk '/renamed:/ {print $2, $3, $4}')
-git restore --staged .
+diff_output=$(git diff HEAD)
+#new_files=$(git status | awk '/new file:/ {print $NF}')
+#renamed_files=$(git status | awk '/renamed:/ {print $2, $3, $4}')
+#git restore --staged .
 
 echo "$diff_output"
-echo ""
-echo "$new_files"
-echo ""
-echo "$renamed_files"
+#echo ""
+#echo "$new_files"
+#echo ""
+#echo "$renamed_files"
